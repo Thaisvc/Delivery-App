b04dd6e207430fcd4b61eb239d973f231b67b010
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-01-20 18:20:41 -0300

    Configuração inicial do sequelize

---
+++ back-end/src/database/models/sales_products.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const SaleProduct = sequelize.define('SaleProduct', {
+    saleId: { type: DataTypes.INTEGER, foreignKey: true, allowNull: false },
+    productId: { type: DataTypes.INTEGER, foreignKey: true, allowNull: false },
+    quantity: { type: DataTypes.INTEGER, allowNull: false },
+  },
+    {
+      timestamps: false,
+      tableName: 'sales_products',
+      underscored: true,
+    });
+    SaleProduct.associate = (models) => {
+      models.Sale.belongsToMany(models.Sale, {
+        as: 'sales',
+        through: SaleProduct,
+        foreignKey: 'saleId',
+        otherKey: 'productId',
+      });
+      
+      models.Product.belongsToMany(models.Product, {
+        as: 'products',
+        through: SaleProduct,
+        foreignKey: 'productId',
+        otherKey: 'saleId',
+      });
+    }
+
+  return SaleProduct;
+};
