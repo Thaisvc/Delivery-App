bbf9199dde83435251ae035c95302935048c5e26
Sarah Lana Reis <sahreis16@gmail.com>
2023-01-23 20:07:45 -0300

    Alterando Routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AppProvider from './provider/AppProvider';
 import Products from './pages/Products';
 
 function App() {
   return (
     <AppProvider>
-      <Router>
-        <Route path="/products" element={ <Products /> } />
-      </Router>
+      <Routes>
+        <Route path="/customer/products" element={ <Products /> } />
+      </Routes>
     </AppProvider>
   );
 }
