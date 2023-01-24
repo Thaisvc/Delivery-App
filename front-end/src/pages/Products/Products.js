881f837df66a580ec88c4e945ba288201b8d189a
Sarah Lana Reis <sahreis16@gmail.com>
2023-01-24 17:29:07 -0300

    Req-12-Criando card de produtos, elementos com os data-testids

--- front-end/src/pages/Products/Products.js
+++ front-end/src/pages/Products/Products.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Products/Navbar';
+import ProductCard from '../../components/Products/ProductCard';
 
 function Products() {
   return (
     <>
       <Navbar />
-      <section className="products-section" />
+      <ProductCard />
     </>
   );
 }
