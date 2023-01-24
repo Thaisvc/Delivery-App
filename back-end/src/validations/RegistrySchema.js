e3601b86b25827a70b888df93830f81065a408b9
thais viana <thaisviana081@outlook.com>
2023-01-24 09:58:19 -0300

    lint 😤

--- back-end/src/validations/RegistrySchema.js
+++ back-end/src/validations/RegistrySchema.js
@@ -4,7 +4,7 @@ const RegistrySchema = Joi.object({
   name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-  role:  Joi.string().required(),
+  role: Joi.string().required(),
 });
 
 module.exports = RegistrySchema;
