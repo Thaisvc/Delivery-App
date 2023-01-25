aef9cbd2f86f5dd8ca90ae095bbd7e15c714c1af
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-01-25 09:22:27 -0300

    Conflitos de merge e teste completo das feats até o req-14

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,7 +5,8 @@ const registryRouter = require('../routes/registryRouter');
 const ErrorHandler = require('../middlewares/ErrorHandler');
 const productRouter = require('../routes/productRouter');
 
-const path = '/home/vinicius/Turma_22_B/Projetos/Back-end/sd-022-b-project-delivery-app/back-end/src/images'
+const path = (
+'/home/vinicius/Turma_22_B/Projetos/Back-end/sd-022-b-project-delivery-app/back-end/src/images');
 
 const app = express();
 app.use(express.json());
